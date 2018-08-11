a36e40b985662c8e320f80f907fdd377c557a3a5
Dmitriy <devilprime5@gmail.com>
2018-08-11 15:20:53 +0600

    new life of repository

--- GameProject/Assets/Scripts/playerController.cs
+++ GameProject/Assets/Scripts/playerController.cs
@@ -5,41 +5,38 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
     public float speed = 20f;
+    public float rotation = 90f;
+    public Transform shipModel; // сслыка на модель корабля (должна быть дочерним объектом) 
     private Rigidbody2D rb;
+    private Quaternion quaternion; // значение вращения корпуса по умолчанию 
+    private float shipRotation = 0;     // текущий поворот корпуса 
     private bool faceRight = true;
     Animator animator;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>(); // получаем экземпляр Rigitbody2D
+        quaternion = shipModel.localRotation; // сохраняем значение вращения корпуса по умолчанию 
         animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
         float moveX = Input.GetAxis("Horizontal"); // присваеваем от 1 до -1
-        rb.MovePosition (position: rb.position + Vector2.right * moveX * speed * Time.deltaTime);  // двигаем таил
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(moveX * speed * Time.deltaTime, moveY * speed * Time.deltaTime);
+        rb.MovePosition (position: rb.position + direction);  // двигаем таил
+        shipRotation = Mathf.Lerp(shipRotation, -moveX * 360, 0.1f);
+        shipModel.localRotation = quaternion * Quaternion.AngleAxis(shipRotation, new Vector3(0,0,1));
         if (Input.GetAxis("Horizontal") == 0)
         {
-            flip();
             animator.SetInteger("stayt", 1);
         } else
         {
-            flip();
             animator.SetInteger("stayt", 2);
         }
         if (Input.GetKeyDown(KeyCode.Space)) // проверка на пробел
         {
             animator.SetInteger("stayt", 3);
-            rb.AddForce(Vector2.up * 8000);
+            //rb.AddForce(Vector2.up * 8000);
         }
 	}
-
-    void flip()
-    {
-        if (Input.GetAxis("Horizontal") > 0)
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        if (Input.GetAxis("Horizontal") < 0)
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-        //        faceRight = !faceRight;
-        //        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    }
 }
