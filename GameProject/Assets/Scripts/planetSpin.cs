7a6237f0e18a05d66ec938f018dbcc0275494dcf
soberLord <zvvlad2@gmail.com>
2018-08-12 13:21:55 +0700

    add spin on cicle

--- GameProject/Assets/Scripts/planetSpin.cs
+++ GameProject/Assets/Scripts/planetSpin.cs
@@ -7,14 +7,13 @@ public class planetSpin : MonoBehaviour {
 	public Transform shipModel;
 	private Quaternion _shipDefaultRotation;
 	private float speedSpin = 0.7f;
-	// Use this for initialization
 	void Start () {
 		_shipDefaultRotation = shipModel.localRotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.position += 20;
 		transform.rotation *= Quaternion.Euler(0f, 0f, speedSpin);
+		transform.Translate(new Vector3(0.3f, 0.3f, 0.0f));
 	}
 }
