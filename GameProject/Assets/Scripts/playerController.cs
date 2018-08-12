b400a84bed79fbf7cb36ee2a0384096e467466aa
denis <yk_d@mail.ru>
2018-08-12 11:35:24 +0600

    delete comment

--- GameProject/Assets/Scripts/playerController.cs
+++ GameProject/Assets/Scripts/playerController.cs
@@ -1,16 +1,7 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-using System;
+﻿using System;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
-    /*public float speed = 20f;
-    public float rotationSpeed = 90f;
-    public Transform shipModel; // сслыка на модель корабля (должна быть дочерним объектом) 
-   // private Rigidbody2D rb;
-    private Quaternion quaternion; // значение вращения корпуса по умолчанию 
-    private float shipRotation = 0;     // текущий поворот корпуса 
-    private float shipSpeed = 0;*/
     Animator animator;
     public float speed = 5; // максимальная скорость корабля в секунду 
     public float speedDamping = 0.1f; // дампинг скорости 
@@ -20,7 +11,6 @@ public class playerController : MonoBehaviour {
     public float maxRotationAngle = 360f; // максимальный укол крена корпуса 
 
     public Transform shipModel; // сслыка на модель корабля (должна быть дочерним объектом) 
-
 
     // значение вращения корпуса по умолчанию 
     private Quaternion _shipDefaultRotation;
@@ -32,8 +22,6 @@ public class playerController : MonoBehaviour {
 
 
     void Start () {
-        // rb = GetComponent<Rigidbody2D>(); // получаем экземпляр Rigitbody2D
-        //quaternion = shipModel.localRotation; // сохраняем значение вращения корпуса по умолчанию 
         animator = GetComponent<Animator>();
         _shipDefaultRotation = shipModel.localRotation;
     }
@@ -50,29 +38,14 @@ public class playerController : MonoBehaviour {
 
         // рассчитываем поврот корпуса корабля с дампингом 
         _shipRotation = Mathf.Lerp(_shipRotation, rot * maxRotationAngle, rotationDamping);
-        // рассчитываем значение поворота корпуса 
-        // shipModel.localRotation = _shipDefaultRotation * Quaternion.AngleAxis(_shipRotation, new Vector3(0,0,1));
-        /*// скорость корабля с дампингом 
-        shipSpeed = Mathf.Lerp(shipSpeed, Mathf.Max(Input.GetAxis("Vertical"), 0), 0.1f);
-        // смещаем корабль в перед на нужную величину 
-        transform.position += transform.position * shipSpeed * Time.deltaTime;
-
-        float rot = Input.GetAxis("Horizontal");
-        // вращаем корабль по оси Z 
-        transform.rotation *= Quaternion.AngleAxis(rot * rotationSpeed * Time.deltaTime, transform.up);
-
-        // рассчитываем поврот корпуса корабля с дампингом 
-        shipRotation = Mathf.Lerp(shipRotation, rot * 30, 0.1f);
-        // рассчитываем значение поворота корпуса 
-        shipModel.localRotation = quaternion * Quaternion.AngleAxis(shipRotation, Vector3.back);*/
+        
         if (Input.GetAxis("Vertical") != 0)
         {
-            //animator.SetInteger("condition", 1);
             animator.SetInteger("condition", 2);
         } 
         else
         {
-
+            animator.SetInteger("condition", 1);
         }
     }
 }
